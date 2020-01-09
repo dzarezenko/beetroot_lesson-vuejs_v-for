00887a6cbe9010d0c0ca479c2102a22b66baf0a1
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2020-01-09 10:21:55 +0200

    v-for object iteration examples

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    
+    <ol>
+      <li v-for="(person, index) in persons" :key="index">
+        <span v-for="(prop, key, id) in person" :key="id">, {{key}}: {{ prop }} ({{ id }})</span>
+      </li>
+    </ol>
+
+    <span v-for="n in 40" :key="n">{{ n }}&nbsp;</span>
   </div>
 </template>
 
@@ -8,8 +14,23 @@
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      persons: [
+        {
+          name: "Anna",
+          age: 99,
+        },
+        {
+          name: "Dmytro",
+          age: 33,
+        },
+        {
+          name: "Illya",
+          age: 23,
+        },
+      ],
+    }
+  },
 }
 </script>
